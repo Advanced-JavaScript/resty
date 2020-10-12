c89bd981c8d0bc47e2fde4810491f612bc4844c2
aalbarqi <ashjanalbarqi2@gmail.com>
2020-10-12 15:56:56 +0300

    Updates

---
+++ precache-manifest.c957b42fb62d068311f6360876d212e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0af531cf986e44968a223c6554713ee",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "76b84fc71f80ab9416f4",
+    "url": "/resty/static/css/main.4949bdcd.chunk.css"
+  },
+  {
+    "revision": "3f0c54749b768ef2638b",
+    "url": "/resty/static/js/2.7b3ead73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.7b3ead73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b84fc71f80ab9416f4",
+    "url": "/resty/static/js/main.706811cb.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
