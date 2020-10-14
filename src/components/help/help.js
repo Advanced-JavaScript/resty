3f22664184959a7f6b2f3c454b4e23a8684e7622
aalbarqi <ashjanalbarqi2@gmail.com>
2020-10-14 22:54:42 +0300

    edit

---
+++ src/components/help/help.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './help.scss';
+/**
+ * @component Help
+ */
+function Help() {
+
+  return (
+    <>
+      <h3>Do you need help?!</h3>
+      <h4>We're here to help you</h4>
+      <p>Steps: <br/> - Enter the URL in the empty input box
+        <br/> - Choose the method from the methods availabel
+        <br/> - Enter the Body in the etextarea box
+        <br/> - Click submit
+      </p>
+    </>
+  );
+}
+
+export default Help;
